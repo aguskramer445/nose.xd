204315d346dd07c0a49fef798345c0fd04cb5cc3
aguskramer445 <136341916+aguskramer445@users.noreply.github.com>
2023-08-25 07:36:07 -0300

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,14 @@
+function calcularPromedio(a,b,c)
+{
+  let promedio = (a + b + c) /3
+    return promedio
+}
+console.log(calcularPromedio(10,9,8))
+
+//cubo
+function calcularCubo(a)
+{
+    let cubo = a * a * a
+    return cubo
+}
+console.log(calcularCubo(9))
